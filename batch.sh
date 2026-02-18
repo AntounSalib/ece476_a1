fb958e6c2f8059db5e561323146261a9bc709f6f
evelyntaliaferro <et0328@princeton.edu>
2026-02-17 19:47:39 -0500

    p1 timing runs + p3 + some p4

--- batch.sh
+++ batch.sh
@@ -3,10 +3,10 @@
 #SBATCH --exclusive
 #SBATCH --ntasks=1
 #SBATCH --time=00:09:00
-#SBATCH --output=sbatch/SLURM-mandelbrot-C2_2026-02-16_23-38-52.log
+#SBATCH --output=sbatch/SLURM-mandelbrot-C2_2026-02-17_19-44-24.log
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=2
 #SBATCH --job-name=mandelbrot-C2
 #SBATCH --distribution=block:block
 #SBATCH --constraint=skylake
-./prog1_mandelbrot_threads/mandelbrot -t 2 -i 1 2> logs/mandelbrot-C2.log
+./prog1_mandelbrot_threads/mandelbrot -t 2 -i 0 2> logs/mandelbrot-C2.log
