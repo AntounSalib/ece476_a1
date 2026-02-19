eae3747e524b93f5c615924a528b4cc853c36846
evelyntaliaferro <et0328@princeton.edu>
2026-02-18 23:03:34 -0500

    committttttt

--- batch.sh
+++ batch.sh
@@ -4,10 +4,10 @@
 #SBATCH --exclusive
 #SBATCH --ntasks=1
 #SBATCH --time=01:01:00
-#SBATCH --output=sbatch/SLURM-sqrt-C32_skylake_2026-02-18_21-12-04.log
+#SBATCH --output=sbatch/SLURM-plot.py-C1_skylake_2026-02-18_22-47-34.log
 #SBATCH --mem=16G
-#SBATCH --cpus-per-task=32
-#SBATCH --job-name=sqrt-C32
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=plot.py-C1
 #SBATCH --distribution=block:block
 #SBATCH --constraint=skylake
-./prog4_sqrt/sqrt -t 32 2> logs/sqrt-C32.log
+./prog6_kmeans/plot.py -t 1 2> logs/plot.py-C1.log
